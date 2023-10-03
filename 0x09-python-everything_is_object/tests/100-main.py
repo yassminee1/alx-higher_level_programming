13d2074924a07443f47dbb7efa96d625fa32d3c0
yassminee <yassmine00elgh@gmail.com>
2023-10-02 23:39:11 -0700

    my commit

---
+++ 0x09-python-everything_is_object/tests/100-main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+magic_string = __import__('100-magic_string').magic_string
+
+for i in range(10):
+    print(magic_string())
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                                            0,1           All
